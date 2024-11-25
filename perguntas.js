82409155816a5bd924cc0705fd919015cb781535
GRG3B <00001059967066sp@al.educacao.sp.gov.br>
2024-11-25 09:51:30 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,53 @@
+criaCartao (
+    'Urotsuki',
+    'O que o nome da personagem principal significa?',
+    'O nome da personagem principal pode ser interpretado como "Urotsuku", que significa "Vagar sem rumo".'
+)
+
+criaCartao (
+    'Urotsuki',
+    'Qual seria a marca mais reconhecível da personagem?',
+    'A marca mais reconhecível da personagem seria o padrão xadrez de sua camisa (Fundo da página).'
+)
+
+criaCartao (
+    'urotsuki',
+    'A personagem é referida como uma "hikikomori", oque isso sigifica?',
+    'Hikikomori é um transtorno mental marcado por isolamento social grave.'
+)
+
+criaCartao (
+    'Efeitos',
+    'O que é um efeito?',
+    'Efeitos são diversos items que mudam a aparência da urotsuki e, em alguns casos, lhe fornecem certas habilidades.'
+)
+
+criaCartao (
+    'Efeitos',
+    'Quantos efeitos existem atualmente?',
+    'Existem 35 efeitos no jogo atualmente.'
+)
+
+criaCartao (
+    'Efeitos',
+    'O que são as máscaras?',
+    'Máscaras são pseudo-efeitos, mudando apenas a aparência da urotsuki temporariamente.'
+)
+
+criaCartao (
+    'Mundo dos sonhos',
+    'O que é o "nexus"?',
+    'O nexus é o mundo que conecta diretamente o quarto da urostuski com o mundo dos sonhos.'
+)
+
+criaCartao (
+    'Coletáveis',
+    'Quais são os coletáveis do jogo?',
+    'Você pode coletar efeitos, papeís de parede e temas de menu.'
+)
+
+criaCartao (
+    'Jogo',
+    'Quantos finais existem no jogo?',
+    'Existem 8 finais no jogo, cada um exigindo diferentes ações e coletáveis.'
+)
